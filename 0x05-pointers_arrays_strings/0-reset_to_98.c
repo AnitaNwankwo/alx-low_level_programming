92b6dbfc074d91a2b3918a50053b1d74334bfb28
AnitaNwankwo <anitanawankwochi@gmail.com>
2023-03-28 14:28:06 +0100

    pointer variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset to 98
+ * @n: pointer passed
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Introduction to DSA - Pointers and arrays
