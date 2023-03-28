4b7e879c3ad6c94ed86b5e93cd3ed13b9c56dabe
AnitaNwankwo <anitanawankwochi@gmail.com>
2023-03-28 17:11:27 +0100

    swapping numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swapping two pointer variables
+ * @a: pointer variable one
+ * @b: pointer variable two
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c; 
+}
